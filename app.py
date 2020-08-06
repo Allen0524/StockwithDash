d168349c738dde9ba0d5893c66d2413a1dd49c14
Allen0524 <69179301+Allen0524@users.noreply.github.com>
2020-08-06 19:52:46 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,209 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State, ALL, MATCH
+import plotly.express as px
+import pandas as pd
+import datetime
+from data import Data
+import sys
+import os
+import sqlite3
+import dash_table
+import math
+#import talib
+import plotly.graph_objects as go
+import pandas_datareader as pdr
+#from talib import abstract
+import plotly.express as px
+from  formulation import getPB, getOneSeasonEPS, getMonthRevenue, getCashFlow, getIncomeTable
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__)
+
+row = html.Div([
+    html.Div([
+        html.Div([
+            html.Div(children=[
+                    html.Div(["收盤價"], className="card-header"),
+                    html.Div([
+                        html.H4(id='sPrice', className="card-title")
+                    ], className="card-body")
+                ], className="card text-white bg-secondary mb-3")
+        ], className="bs-component")
+    ], className="col-lg-2"),
+    html.Div([
+        html.Div([
+            html.Div(children=[
+                    html.Div(["EPS"], className="card-header"),
+                    html.Div([
+                        html.H4(id='sEPS', className="card-title")
+                    ], className="card-body")
+                ], className="card text-white bg-secondary mb-3")
+        ], className="bs-component")
+    ], className="col-lg-2"),
+    html.Div([
+        html.Div([
+            html.Div(children=[
+                    html.Div(["本益比"], className="card-header"),
+                    html.Div([
+                        html.H4(id='sPER', className="card-title")
+                    ], className="card-body")
+                ], className="card text-white bg-secondary mb-3")
+        ], className="bs-component")
+    ], className="col-lg-2"),
+    html.Div([
+        html.Div([
+            html.Div(children=[
+                    html.Div(["股價淨值比"], className="card-header"),
+                    html.Div([
+                        html.H4(id='sPBR', className="card-title")
+                    ], className="card-body")
+                ], className="card text-white bg-secondary mb-3")
+        ], className="bs-component")
+    ], className="col-lg-2"),
+    html.Div([
+        html.Div([
+            html.Div(children=[
+                    html.Div(["成交股數"], className="card-header"),
+                    html.Div([
+                        html.H4(id='sNumber', className="card-title")
+                    ], className="card-body")
+                ], className="card text-white bg-secondary mb-3")
+        ], className="bs-component")
+    ], className="col-lg-2"),
+    html.Div([
+        html.Div([
+            html.Div(children=[
+                    html.Div(["收盤日"], className="card-header"),
+                    html.Div([
+                        html.H4(id='sClose', className="card-title")
+                    ], className="card-body")
+                ], className="card text-white bg-secondary mb-3")
+        ], className="bs-component")
+    ], className="col-lg-2")
+], className="row", style={"padding-top":"20px", "padding-left":"10px"})
+
+
+row1 = html.Div([
+    html.Div([
+        html.Div([
+            html.Div(children=[
+                    html.Div(["每月營收"], className="card-header"),
+                    html.Div([
+                        dcc.Graph(
+                            id='monthRevenue-fig', figure={}
+                        )
+                    ], className="card-body")
+                ], className="card border-secondary mb-3")
+        ], className="bs-component")
+    ], className="col-lg-6"),
+    html.Div([
+        html.Div([
+            html.Div(children=[
+                    html.Div(["單季EPS"], className="card-header"),
+                    html.Div([
+                        dcc.Graph(
+                            id='oneSeasonEPS-fig', figure={}
+                        )
+                    ], className="card-body")
+                ], className="card border-secondary mb-3")
+        ], className="bs-component")
+    ], className="col-lg-6"),
+    html.Div([
+        html.Div([
+            html.Div(children=[
+                    html.Div(["現金流量表"], className="card-header"),
+                    html.Div([
+                        dcc.Graph(
+                            id='cashFlowOperating-fig', figure={}
+                        )
+                    ], className="card-body")
+                ], className="card border-secondary mb-3")
+        ], className="bs-component")
+    ], className="col-lg-6"),
+    html.Div([
+        html.Div([
+            html.Div(children=[
+                    html.Div(["損益表"], className="card-header"),
+                    html.Div([
+                        dcc.Graph(
+                            id='incomeStatement-fig', figure={}
+                        )
+                    ], className="card-body")
+                ], className="card border-secondary mb-3")
+        ], className="bs-component")
+    ], className="col-lg-6")
+], className="row")
+
+
+
+app.layout = html.Div([
+    html.Nav([
+        html.A(children="stock ^.^", href='#', className='navbar-brand'),
+        html.Div([
+            html.Ul([
+                html.Li([
+                    html.H3(id='sName', children="")
+                ], className="nav-item active")
+            ], className="navbar-nav mr-auto"),
+            html.Form([
+                dcc.Input(id='sId-Input', value='2330', type='number', placeholder='StockId', className="form-control mr-sm-2"),
+                html.Button(id='sId-Btn', children="search", n_clicks=0, className="btn btn-secondary my-2 my-sm-0")
+            ], className="form-inline my-2 my-lg-0")
+        ], className="collapse navbar-collapse")
+
+    ], className="navbar navbar-expand-lg navbar-dark bg-primary"),
+    html.Div([
+        row,
+        row1,
+    ], className="container"),
+], style={'background':"#323B44"})
+
+
+
+@app.callback(
+    [Output('sName', 'children'),
+    Output('sPrice', 'children'),
+    Output('sNumber', 'children'),
+    Output('sPER', 'children'),
+    Output('sPBR', 'children'),
+    Output('sEPS', 'children'),
+    Output('sClose', 'children'),
+    Output('monthRevenue-fig', 'figure'),
+    Output('oneSeasonEPS-fig', 'figure'),
+    Output('cashFlowOperating-fig', 'figure'),
+    Output('incomeStatement-fig', 'figure')],
+    [Input('sId-Btn', 'n_clicks')],
+    [State('sId-Input', 'value')]
+)
+def update_basic_info(n_clicks, input_value):
+    
+    data = Data()
+    conn = sqlite3.connect('dataBase.db')
+    strid = str(input_value)
+
+    sPrice = data.get('收盤價', 1)[strid].values[0]
+    sName = data.get('公司名稱', 1)[strid]
+    sNumber = format(data.get('成交股數', 1)[strid].values[0], ',')
+    sPER = data.get('本益比', 1)[strid].values[0]
+    sClose = data.dates['price'].iloc[-1].name.strftime("%Y/%m/%d")
+    sPBR = getPB(strid)
+    sEPS = data.get('基本每股盈餘合計', 1)[strid].values[0]
+
+    #figure
+    revenueFig = getMonthRevenue(strid)
+    epsFigure = getOneSeasonEPS(strid)
+    chasflowFigure = getCashFlow(strid)
+    incometableFigure = getIncomeTable(strid)
+
+    
+    
+    
+    return sName, sPrice, sNumber, sPER, sPBR, sEPS, sClose, revenueFig, epsFigure, chasflowFigure, incometableFigure
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
